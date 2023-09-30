a86de6dc77c449d8d36eea0ba98f3c79812aeb7c
Nabaraj <nabaraj.dhl77@gmail.com>
2023-09-30 13:47:00 +0545

    Done

---
+++ Routes/Message.route.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+const MessageController = require("../Controllers/Message.controller");
+
+//Fetch Projects
+router.get("/messages", MessageController.getMessages);
+//Post Messages
+router.post("/messages", MessageController.postMessages);
+
+module.exports = router;
